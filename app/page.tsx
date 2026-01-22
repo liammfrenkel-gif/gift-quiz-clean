da49605204ac2003a03e291124ea534d0fae01b4
liammfrenkel-gif <liammfrenkel@gmail.com>
2026-01-22 17:26:02 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Gift Quiz 🎁</h1>
+      <p>If you see this, it worked.</p>
+    </main>
+  );
+}
